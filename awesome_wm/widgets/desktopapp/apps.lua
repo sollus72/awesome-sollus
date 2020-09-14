f2be4b6c2bd2a1da6e9f5e3505b4ad452e1ec4c6
sollus72 <48010958+sollus72@users.noreply.github.com>
2020-09-14 07:43:16 +0300

    Update apps.lua

--- awesome_wm/widgets/desktopapp/apps.lua
+++ awesome_wm/widgets/desktopapp/apps.lua
@@ -60,9 +60,9 @@ function desktop_menu ()
 		      desktop_apps_list = {}
 		        for str in f1:lines() do
 
-					-- local pos1, pos2 = string.find(str,".desktop")
+					-- local pos1, pos2 = string.find(str,"[.]desktop")
 					-- local w = string.sub(str,1,pos1-1) .. string.sub(str,pos2+1)
-					local w = string.gsub(str, ".desktop", "")
+					local w = string.gsub(str, "[.]desktop", "")
 
 					-- local pos1, pos2 = string.find(str,"/usr/share/applications/")
 					-- local ww = string.sub(w,1,pos1-1) .. string.sub(w,pos2+1)
