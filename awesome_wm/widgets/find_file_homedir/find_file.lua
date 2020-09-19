49eecf70c3e8c526be5e9a35f3bef74cd3cdeec2
sollus72 <48010958+sollus72@users.noreply.github.com>
2020-09-19 07:47:24 +0300

    Update find_file.lua

--- awesome_wm/widgets/find_file_homedir/find_file.lua
+++ awesome_wm/widgets/find_file_homedir/find_file.lua
@@ -1,38 +1,40 @@
+--[[Finding a file in your home directory
+Dependencies:
+xdg-utils (for xdg-open)
+perl-file-mimeinfo
+font - ttf-play (AUR) or use another
+--]]
 function find_file()
     awful.prompt.run {
         prompt       = '<b>Find Name: </b>',
-        -- text         = 'default command',
-        -- bg_cursor    = '#ff0000',
         bg_cursor    = '#FFFF00',   -- yellow
-        -- To use the default rc.lua prompt:
         textbox      = mouse.screen.mypromptbox.widget,
-        -- textbox      = atextbox,
         exe_callback = function(input)
             if not input or #input == 0 then return end
-            naughty.notify{ text = 'The input was: '..input, timeout = 0, hover_timeout = 0.1 }
+                naughty.notify{ text = 'Find Name: '..input, timeout = 0, hover_timeout = 0.1 }
 
-            local f1 = io.popen('cd  ${XDG_HOME:-$HOME} && find . -iname ' .. '"*' .. input .. '*"')
+                local f1 = io.popen('cd  ${XDG_HOME:-$HOME} && find . -iname ' .. '"*' .. input .. '*"')
                 
-                words = {}
-                for w in f1:lines() do
-                  local ww = string.sub(w, 3)
-                words[#words + 1] = ww
-                end
+                    local words = {}
+                    for w in f1:lines() do
+                      local ww = string.sub(w, 3)
+                    words[#words + 1] = ww
+                    end
                       
-                      nets = {}
+                      local nets = {}
                       for i, c in pairs(words) do
                         nets[i] =
                           { c, function() awful.spawn.with_shell('xdg-open ${XDG_HOME:-$HOME}/' .. '"' .. c .. '"' ) end, find_icon }
                       end
                 
                 awful.menu({items = nets , 
-                            theme = { font = 'Play 10', 
-                                      width = 800, 
-                                      height = 19,
-                                      border_color = '#6F6F6F', border_width = 2,
-                                      bg_normal = '#3F3F3F', bg_focus = '#6F6F6F' 
-                                  }
-                            }):toggle()
+                        theme = { font = 'Play 10', 
+                                  width = 1400, 
+                                  height = 19,
+                                  border_color = '#6F6F6F', border_width = 2,
+                                  bg_normal = '#3F3F3F', bg_focus = '#6F6F6F' 
+                              }
+                        }):toggle()
 
             end
                     }
